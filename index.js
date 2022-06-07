4fe69af1b9a568a3b8d41098ecf5cdd01512a9e8
George Jauga <georgejauga37@gmail.com>
2022-06-08 00:08:28 +0300

    commit tree 763587cbba3681c7a667ccccde55865e9bf87ce4

--- index.js
+++ index.js
@@ -5,28 +5,38 @@ function printValue(value){
 
 // 1.0 DEFINE VARIABLES
 //TODO: ADD fixed variable
-
+const pi = 3.142
+//printValue(pi)
 
 //TODO: ADD redeclarable variable
+var radius = 3.142
+var radius = 30
+//printValue(radius)
 
 
 //TODO: ADD re-assignable but not redeclarable variable
-
+let age = 21
+ age = 19 
+//printValue(age)
 
 // 2.0 DIFFERENT DATA TYPES
 //TODO: Create a fixed variable with a number value
-
+const num = 10
 
 //TODO: Create a redeclarable variable with a boolean value
-
-
+var radius = true
+//printValue(radius)
 //TODO: Create a re-assignable variable with a string value
-
+let firstName = "Jijo"
+let lastName = "Jauga"
+//printValue(lastName)
 
 
 // 3.0 STRING OPERATIONS
 //TODO: Concatenate string (+)
 
+let fullName = firstName +' ' + lastName
+//printValue(fullName)
 
 //TODO: Interpolate string (`${}`)
 
